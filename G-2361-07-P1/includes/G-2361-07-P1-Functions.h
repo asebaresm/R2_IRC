06d252e039eb530e83d47abd618b7e12ccad5a81
Celia <celia_mateos@hotmail.com>
2017-04-18 23:53:01 +0200

    Servidor Terminado Falta Organizar Mas

---
+++ G-2361-07-P1/includes/G-2361-07-P1-Functions.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <syslog.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <redes2/irc.h>
+#include "../includes/G-2361-07-P1-Sockets.h"
+#include "../includes/G-2361-07-P1-Server.h"
+
+
+void funcionNick(long id, char* usuario, char* nick_name, char* real, char* nick, int IDsocket);
+void funcionUser(char *user, char *nick, char *realname, char *modehost, int IDsocket);
+void funcionAway(long id, char *usuario, char *nick_name, char *real, char *away, char *msg, int IDsocket);
+void funcionPart(char *channel, char *nick_name, char *msg, int IDsocket);
+void funcionList(char *nick, char *target, int IDsocket);
+void funcionMode(char *usuario, char *nick_name, char *channeluser, char *modo, char *key, int IDsocket);
+void funcionMotd(char *nick_name, char *target, int IDsocket, char *fich);
